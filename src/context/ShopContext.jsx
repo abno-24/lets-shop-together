b32467d2ce5b668db8c1a11f50fdc33094be13cb
Abhinav Maurya <abhinavmaurya605@gmail.com>
2025-12-31 21:08:29 +0530

    feat: enhance Cart and PlaceOrder pages with checkout navigation and order summary

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useState } from "react";
 import { products } from "../assets/assets";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const ShopContext = createContext();
@@ -11,6 +12,7 @@ const ShopContextProvider = (props) => {
   const [search, setSearch] = useState('');
   const [showSearch, setShowSearch] = useState(false);
   const [cartItems, setCartItems] = useState({});
+  const navigate = useNavigate();
 
   const addToCart = async (itemId, size) => {
     if (!size) {
@@ -56,7 +58,7 @@ const ShopContextProvider = (props) => {
     setCartItems(cartData);
   }
 
-  const getCartAmount = async () => {
+  const getCartAmount = () => {
     let totalAmount = 0;
     for (const items in cartItems) {
       let itemInfo = products.find((product) => product._id === items);
@@ -85,7 +87,8 @@ const ShopContextProvider = (props) => {
     addToCart,
     getCartCount,
     updateQuantity,
-    getCartAmount
+    getCartAmount,
+    navigate
   }
 
   return (
