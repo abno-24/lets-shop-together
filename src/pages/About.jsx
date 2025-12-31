2a5b5c707599e2414f6aa8659d6ec0601f233e4e
Abhinav Maurya <abhinavmaurya605@gmail.com>
2025-12-31 23:16:00 +0530

    feat: enhance About page with title and image layout

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import { assets } from '../assets/assets';
+import Title from '../components/Title';
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <div className='text-2xl text-center pt-8 border-t'>
+        <Title text1={'ABOUT'} text2={'US'} />
+      </div>
+      <div className='my-10 flex flex-col md:flex-row gap-16'>
+        <img className='w-full md:max-w-112.5' src={assets.about_img} alt="" />
+        <div className='flex'></div>
+      </div>
+    </div>
   )
 }
 
