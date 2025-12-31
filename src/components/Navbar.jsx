dd83565aae2ca5604b114e30b2e62c2ce8e5f354
Abhinav Maurya <abhinavmaurya605@gmail.com>
2025-12-31 12:27:15 +0530

    feat: add SearchBar component and integrate search functionality in Navbar and ShopContext

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { assets } from "../assets/assets"
 import { Link, NavLink } from 'react-router-dom'
+import { ShopContext } from '../context/ShopContext';
 
 const Navbar = () => {
+  const { showSearch, setShowSearch } = useContext(ShopContext);
   const [visible, setVisible] = useState(false);
 
   return (
@@ -31,7 +33,7 @@ const Navbar = () => {
       </ul>
 
       <div className='flex items-center gap-6'>
-        <img src={assets.search_icon} className='w-5 cursor-pointer' alt="search icon" />
+        <img onClick={() => setShowSearch(!showSearch)} src={assets.search_icon} className='w-5 cursor-pointer' alt="search icon" />
 
         <div className='group relative'>
           <img src={assets.profile_icon} className='w-5 cursor-pointer' alt="profile icon" />
