6e74028e01dcce534d711358f4a1b304a4d5112d
Abhinav <abhinavmaurya605@gmail.com>
2026-01-01 12:24:05 +0530

    feat: enhance Contact page with additional store information and layout improvements

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Title from '../components/Title'
 import { assets } from '../assets/assets'
+import NewsLetter from '../components/NewsLetter'
 
 const Contact = () => {
   return (
@@ -10,7 +11,20 @@ const Contact = () => {
        </div>
        <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28'>
         <img className='w-full md:max-w-120' src={assets.contact_img} alt="Contact Image" />
+        <div className='flex flex-col justify-center items-start gap-6'>
+          <p className='font-semibold text-xl text-gray-600'>Our Store</p>
+          <p className='text-gray-500'>
+            1234 Elm Street,<br /> Springfield, IL 62704<br /> United States
+          </p>
+          <p className='text-gray-500'>Tel: (123) 456-7890 <br />Email: hello@example.com</p>
+          <p className='font-semibold text-xl text-gray-600'>Careers at Forever</p>
+          <p className='text-gray-500'>Learn more about our teams and job openings.</p>
+          <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500 cursor-pointer'>
+            Explore Jobs
+          </button>
+        </div>
        </div>
+       <NewsLetter />
     </div>
   )
 }
