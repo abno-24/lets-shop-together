0f4c1b275a6445912167eadb8cea44ded2906045
Abhinav <abhinavmaurya605@gmail.com>
2026-01-01 12:13:06 +0530

    feat: enhance Contact page with structured layout and image

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/assets'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+       <div className='text-center text-2xl pt-10 border-t'>
+        <Title text1={'CONTACT'} text2={'US'} />
+       </div>
+       <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28'>
+        <img className='w-full md:max-w-120' src={assets.contact_img} alt="Contact Image" />
+       </div>
+    </div>
   )
 }
 
